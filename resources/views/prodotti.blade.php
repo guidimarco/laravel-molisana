9cd95f3da25a3ae5a46692d401ccd7ab25b1b4c2
Marco Guidi <mark.88g@gmail.com>
2020-12-22 16:37:35 +0100

    PRODOTTI card statica pasta

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -4,6 +4,17 @@
 
 @section('content')
     <div class="container">
-        <h1>PRODOTTI</h1>
+        <div class="row card-container">
+            <div class="col-xs-12 col-md-4">
+                <div class="card">
+                    <img src="https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg" alt="">
+                    <div class="card-overlay">
+                        ciao
+                    </div>
+                </div><!-- end card -->
+            </div><!-- end card-container -->
+
+
+        </div>
     </div>
 @endsection
